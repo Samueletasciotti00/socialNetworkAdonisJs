b3e7df7fe064c2c57168955857dab46a4febd968
Samuele <samueletasciotti@icloud.com>
2024-10-27 23:57:43 +0100

    fix: type of "theme" in model.ts

--- app/models/user.ts
+++ app/models/user.ts
@@ -56,7 +56,7 @@ export default class User extends compose(BaseModel, AuthFinder) {
   declare like: number
 
   @column()
-  declare theme: string
+  declare theme: 'light' | 'dark'
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
