df21b3190f657c8591000969f095d580c3679ef0
Samuele <samueletasciotti@icloud.com>
2024-10-28 16:33:08 +0100

    fix: update method in controller

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,33 +1,20 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import User from '../models/user.js'
-// import { DateTime } from 'luxon'
 
 export default class UsersController {
-  /**
-   * Display a list of resource
-   */
+  // Show all data from User()
   async index({ response }: HttpContext) {
-    // Si passano i parametri di richiest e risposta
     try {
       // Save all database users in a variable
       const users = await User.all()
       return response.status(200).json({ data: users })
     } catch (error) {
-      return response.status(500).json({ error: 'Fail index' })
+      return response.status(500).json({ error: 'Fail to return all data from USER' })
     }
   }
 
-  /**
-   * Display form to create a new record
-   */
-  async create({}: HttpContext) {}
-
-  /**
-   * Handle form submission for the create action
-   */
   async store({ request, response }: HttpContext) {
     try {
-
       // Request all data params for new User
       const data = request.only([
         'user',
@@ -52,51 +39,65 @@ export default class UsersController {
 
       // Return for the response
       return response.status(201).json({ data: user, message: 'User created successfully' })
-
     } catch (error) {
-      
-      // Error state
       return response.status(400).json({ error: 'Failed to create a new User' })
     }
   }
 
-  /**
-   * Show individual record
-   */
   async show({ params }: HttpContext) {
     // Show an User details
     return await User.findOrFail(params.id)
+    console.log(1)
   }
 
-  /**
-   * Handle form submission for the edit action
-   */
-  async update({ params, request }: HttpContext) {
-    // Declare the User to be edited in a variable
-    const user = await User.findOrFail(params.id)
+  async update({ params, request, response }: HttpContext) {
+    try {
+      // Request all data params for update specific User
+      const data = request.only([
+        'user',
+        'email',
+        'password',
+        'firstName',
+        'lastName',
+        'dateOfBirth',
+        'gender',
+        'location',
+        'profileImgUrl',
+        'coverImgUrl',
+        'bio',
+        'profileVisibility',
+        'post',
+        'like',
+        'theme',
+      ])
 
-    user.user = request.input('user') // Callback userName
-    user.email = request.input('email') // Callback email
-    user.password = request.input('password') // Callback password
+      // Declare the User to be edited in a variable
+      const user = await User.findOrFail(params.id)
 
-    // Save data
-    await user.save()
+      user.merge(data)
 
-    // Return response
-    return user
+      await user.save()
+
+      // Return response
+      return response.status(201).json({ data: user, message: 'User updated successfully' })
+    } catch (error) {
+      return response.status(400).json({ error: 'Failed to update the User' })
+    }
   }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {
-    // Declare the data to be deleted in a variable
-    const user = await User.findByOrFail(params.id)
+  async destroy({ params, response }: HttpContext) {
+    try {
+      // Declare the data to be deleted in a variable
+      const user = await User.findByOrFail(params.id)
 
-    // Delete data
-    await user.delete()
-
-    // Return
-    return user
+      // Delete data
+      await user.delete()
+      return response.status(200).json({ message: 'User deleted successfully' })
+    } catch {
+      return response.status(404).json({ message: 'User not found' })
+    }
   }
 }
