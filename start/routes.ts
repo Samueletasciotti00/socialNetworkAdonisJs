763ced8323c7651f64307dc3d3ff684d429e4d8c
Samuele <samueletasciotti@icloud.com>
2024-10-28 16:34:05 +0100

    fix: clear router

--- start/routes.ts
+++ start/routes.ts
@@ -12,14 +12,9 @@ import router from '@adonisjs/core/services/router'
 // Save the UserController in a const
 const UsersController = () => import('#controllers/users_controller')
 
-// router.get('/', async () => {
-//   return 'Home Page Basic'
-// })
-
-// Iznsert the routes for the UserController
-
+// Insert the routes for the UserController
 router.get('users', [UsersController, 'index']) // Show all data
 router.get('users/:id', [UsersController, 'show']) // Show specific user detail
 router.post('users', [UsersController, 'store']) // Update Database 
-router.patch('user/:id',[UsersController, 'update']) // Update single data
-router.delete('user/:id',[UsersController, 'destroy']) // Delete data
+router.put('users/:id/edit' ,[UsersController, 'update']) // Update single data
+router.delete('users/:id/delete',[UsersController, 'destroy']) // Delete data
