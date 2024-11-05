3084c8d0c9342cd28ba37ac2f09092b6109aafba
Samuele <samueletasciotti@icloud.com>
2024-11-05 12:13:32 +0100

    fix: CreateUserTable

--- database/migrations/1729939155745_create_users_table.ts
+++ database/migrations/1729939155745_create_users_table.ts
@@ -8,8 +8,8 @@ export default class extends BaseSchema {
       table.increments('id').notNullable()
       
       // Login Data
-      table.string('user',15).notNullable().unique()
-      table.string('email', 20).notNullable().unique()
+      table.string('user_name',16).notNullable().unique()
+      table.string('email', 320).notNullable().unique()
       table.string('password', 255).notNullable()
 
       // Info 
@@ -17,18 +17,8 @@ export default class extends BaseSchema {
       table.string('last_name').notNullable()
       table.date('date_of_birth').notNullable()
       table.enum('gender',['male', 'female', 'other']).notNullable()
-      table.string('location')
-
-      // Profile User
-      table.string('profile_img_url') // Insert a default image
-      table.string('cover_img_url').nullable() // Insert a default image
-      table.string('bio')
+      table.string('residence')
       table.enum('profile_visibility', ['public', 'private', 'friends']).defaultTo('public')
-      table.integer('post').defaultTo(0)
-      table.integer('like').defaultTo(0)
-
-      // Detail of profile user
-      table.string('theme').defaultTo('light').notNullable()
 
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at')
